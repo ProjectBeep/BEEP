e2673c36f44405e7d49e830eb68cd737a7a55321
김명석 <audxo112@naver.com>
2023-02-16 18:16:22 +0900

    Issues #287 fix: intro Exception message 로 인한 오류 해결

--- features/ui-intro/src/main/java/com/lighthouse/features/intro/exception/FailedConnectException.kt
+++ features/ui-intro/src/main/java/com/lighthouse/features/intro/exception/FailedConnectException.kt
@@ -1,5 +1,5 @@
 package com.lighthouse.features.intro.exception
 
-class FailedConnectException(
+internal class FailedConnectException(
     message: String = "서버와 연결을 실패 했습니다."
-) : Exception(message = message)
+) : Exception(message)
--- features/ui-intro/src/main/java/com/lighthouse/features/intro/exception/FailedLoginException.kt
+++ features/ui-intro/src/main/java/com/lighthouse/features/intro/exception/FailedLoginException.kt
@@ -2,4 +2,4 @@ package com.lighthouse.features.intro.exception
 
 internal class FailedLoginException(
     message: String = "로그인에 실패 했습니다."
-) : Exception(message = message)
+) : Exception(message)
