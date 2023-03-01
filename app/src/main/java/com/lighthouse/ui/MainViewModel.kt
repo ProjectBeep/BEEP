a50d005f547f0b650db0e3e3ef5b253bd3324cc8
김명석 <audxo112@naver.com>
2023-03-02 00:45:18 +0900

    Issues #287 feat: launchIn 적용

--- app/src/main/java/com/lighthouse/ui/MainViewModel.kt
+++ app/src/main/java/com/lighthouse/ui/MainViewModel.kt
@@ -6,22 +6,21 @@ import com.lighthouse.domain.usecase.user.IsLoginUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
 internal class MainViewModel @Inject constructor(
-    private val isLoginUserUseCase: IsLoginUserUseCase
+    isLoginUserUseCase: IsLoginUserUseCase
 ) : ViewModel() {
 
     private val _isLoginFlow = MutableSharedFlow<Boolean>()
     val isLoginFlow = _isLoginFlow.asSharedFlow()
 
     init {
-        viewModelScope.launch {
-            isLoginUserUseCase().collect { isLogin ->
-                _isLoginFlow.emit(isLogin)
-            }
-        }
+        isLoginUserUseCase()
+            .onEach { _isLoginFlow.emit(it) }
+            .launchIn(viewModelScope)
     }
 }
